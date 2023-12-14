8b53ef5391b77cabd2cfa72e56f17da4b4955061
Andrii Chychkan <mywork1728@gmail>
2023-12-14 23:14:08 +0200

    started recfoodpage

--- src/pages/RecommendedFoodPage/RecommendedFoodPage.jsx
+++ src/pages/RecommendedFoodPage/RecommendedFoodPage.jsx
@@ -1,7 +1,23 @@
+import StyledRecommendedFoodPage from './RecommendedFoodPage.styled';
+
 export default function RecommendedFoodPage() {
+  const food = ['1', '2', '3', '4', '5', 6, 7, 8, 9, 10];
+
   return (
-    <>
-      <p>RecommendedFoodPage</p>
-    </>
+    <StyledRecommendedFoodPage>
+      <div className="main_div">
+        <p>RecommendedFoodPage</p>
+        <div>
+          <div className="img_div">
+            <img src="" alt="" width={300} height={312} />
+          </div>
+          <ul>
+            {food.map((el) => {
+              return <li>{el}</li>;
+            })}
+          </ul>
+        </div>
+      </div>
+    </StyledRecommendedFoodPage>
   );
 }
