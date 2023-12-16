6d9b5b3fd9988e7c1525ab4306c9cedda37ae544
OleksandrKhokhlov <hohlovsanya@gmail.com>
2023-12-16 23:27:36 +0200

    SingInPage

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from 'components/Header/Header';
 import { Container } from './SharedLayout.styled';
+import { Toaster } from 'react-hot-toast';
 
 const SharedLayout = () => {
   return (
@@ -12,6 +13,7 @@ const SharedLayout = () => {
           <Outlet />
         </Suspense>
       </Container>
+      <Toaster reverseOrder={false} />
     </>
   );
 };
