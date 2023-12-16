2624ba74f6635c56c32223ad35b75a2c30029641
MAKCKACHKA <doubleakop@gmail.com>
2023-12-16 20:33:33 +0200

    some fixes

--- src/pages/SettingsPage/SettingsPage.jsx
+++ src/pages/SettingsPage/SettingsPage.jsx
@@ -1,13 +1,207 @@
 import { IconWrapper } from './SettingsPage.styled';
-import Illustration from '../../assets/Illustration.svg';
+import Illustration from '../../assets/pageIllustrations.svg';
+import { useState } from 'react';
 
 export default function SettingsPage() {
+  const [name, setName] = useState('');
+  const [photo, setPhoto] = useState(null);
+  const [age, setAge] = useState('');
+  const [gender, setGender] = useState('');
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
+  const [activity, setActivity] = useState('');
+
+  const handleCancel = () => {
+    setName('');
+    setPhoto(null);
+    setAge('');
+    setGender('');
+    setHeight('');
+    setWeight('');
+    setActivity('');
+  };
+  const handleSave = () => {
+    console.log('Form data saved:', {
+      name,
+      photo,
+      age,
+      gender,
+      height,
+      weight,
+      activity,
+    });
+  };
+
   return (
-    <>
-      <h2>Profile setting</h2>
-      <IconWrapper>
-        <use href={`${Illustration}#icon-Illustration`} />
-      </IconWrapper>
-    </>
+    <div>
+      <div>
+        <div>
+          <h2>Profile setting</h2>
+          <div>
+            <button type="button" onClick={handleCancel}>
+              Cancel
+            </button>
+            <button type="button" onClick={handleSave}>
+              Save
+            </button>
+          </div>
+        </div>
+        <IconWrapper>
+          <use href={`${Illustration}#icon-settings`} />
+        </IconWrapper>
+      </div>
+      <form>
+        <div>
+          <label htmlFor="name">Your name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="photo">Your photo:</label>
+          {photo && <img src={photo} alt="Selected" />}
+
+          <input
+            type="file"
+            accept="image/*"
+            id="photo"
+            name="photo"
+            onChange={(e) => {
+              const file = e.target.files[0];
+              const imageUrl = URL.createObjectURL(file);
+              setPhoto(imageUrl);
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="age">Your age:</label>
+          <input
+            type="number"
+            id="age"
+            name="age"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Gender:</label>
+          <div>
+            <label htmlFor="male">Male</label>
+            <input
+              type="radio"
+              id="male"
+              name="gender"
+              value="Male"
+              checked={gender === 'Male'}
+              onChange={(e) => setGender(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="female">Female</label>
+            <input
+              type="radio"
+              id="female"
+              name="gender"
+              value="Female"
+              checked={gender === 'Female'}
+              onChange={(e) => setGender(e.target.value)}
+            />
+          </div>
+        </div>
+        <div>
+          <label htmlFor="height">Height:</label>
+          <input
+            type="number"
+            id="height"
+            name="height"
+            value={height}
+            onChange={(e) => setHeight(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="weight">Weight:</label>
+          <input
+            type="number"
+            id="weight"
+            name="weight"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Your activity:</label>
+          <div>
+            <input
+              type="radio"
+              id="activity1"
+              name="activity"
+              value="1.2"
+              checked={activity === '1.2'}
+              onChange={(e) => setActivity(e.target.value)}
+            />
+            <label htmlFor="activity1">
+              1.2 - if you do not have physical activity and sedentary work
+            </label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="activity2"
+              name="activity"
+              value="1.375"
+              checked={activity === '1.375'}
+              onChange={(e) => setActivity(e.target.value)}
+            />
+            <label htmlFor="activity2">
+              1.375 - if you do short runs or light gymnastics 1-3 times a week
+            </label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="activity3"
+              name="activity"
+              value="1.55"
+              checked={activity === '1.55'}
+              onChange={(e) => setActivity(e.target.value)}
+            />
+            <label htmlFor="activity3">
+              1.55 - if you play sports with average loads 3-5 times a week
+            </label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="activity4"
+              name="activity"
+              value="1.725"
+              checked={activity === '1.725'}
+              onChange={(e) => setActivity(e.target.value)}
+            />
+            <label htmlFor="activity4">
+              1.725 - if you train fully 6-7 times a week
+            </label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="activity5"
+              name="activity"
+              value="1.9"
+              checked={activity === '1.9'}
+              onChange={(e) => setActivity(e.target.value)}
+            />
+            <label htmlFor="activity5">
+              1.9 - if your work is related to physical labor, you train 2 times
+              a day and include strength exercises in your training program
+            </label>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
--- src/pages/SettingsPage/SettingsPage.styled.jsx
+++ src/pages/SettingsPage/SettingsPage.styled.jsx
@@ -12,41 +12,103 @@ export const Navigation = styled.nav`
   flex-wrap: wrap;
 `;
 
-// export const StyledLink = styled(Link)`
-//   padding: 8px 16px;
-//   color: var(--primary-text-color);
-//   /* text-shadow:
-//     0 0 5px #00baff,
-//     0 0 10px #00baff,
-//     0 0 20px #00baff,
-//     0 0 40px #00baff,
-//     0 0 80px #00baff; */
-//   transition:
-//     color 0.3s,
-//     text-shadow 0.3s;
-
-//   &:hover,
-//   &:focus {
-//     color: #00baff;
-//     /* text-shadow:
-//       0 0 5px #00baff,
-//       0 0 10px #00baff,
-//       0 0 20px #00baff,
-//       0 0 40px #00baff,
-//       0 0 80px #00baff; */
-//   }
-// `;
-
 export const IconWrapper = styled.svg`
-  width: ${(props) => props.size || '512px'};
-  height: ${(props) => props.size || '512px'};
+  width: 536px;
+  height: 528px;
   fill: ${(props) => props.color || '#ffffff'};
   display: inline-block;
   margin-right: 5px;
-  /* box-shadow:
-    0 0 5px #00baff,
-    0 0 10px #00baff,
-    0 0 20px #00baff,
-    0 0 40px #00baff,
-    0 0 80px #00baff; */
+`;
+
+export const HeroSection = styled.section`
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 24px;
+
+  @media screen and (min-width: 768px) {
+    /* margin: 40px auto 80px; */
+    gap: 80px;
+  }
+
+  @media screen and (min-width: 1200px) {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    gap: 104px;
+    justify-content: center;
+  }
+`;
+
+export const HeroImg = styled.svg`
+  width: 300px;
+  height: 296px;
+  display: block;
+  height: 300px;
+  display: block;
+  margin: 0 auto 40px;
+  @media screen and (min-width: 768px) {
+    width: 380px;
+    height: 376px;
+    /* margin: 40px auto 80px; */
+  }
+
+  @media screen and (min-width: 1200px) {
+    width: 592px;
+    height: 588px;
+    margin: 0;
+  }
+`;
+
+export const HeroDescWrap = styled.div`
+  padding-bottom: 40px;
+
+  @media screen and (min-width: 768px) {
+    width: 444px;
+    text-align: center;
+    margin: 0 auto;
+    padding-bottom: 286px;
+  }
+
+  @media screen and (min-width: 1200px) {
+    margin: 0;
+    text-align: left;
+    padding: 0;
+  }
+`;
+
+export const HeroTitle = styled.h1`
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 1.25;
+  margin-bottom: 16px;
+  text-align: center;
+  word-wrap: break-word;
+
+  @media screen and (min-width: 768px) {
+    font-size: 30px;
+    line-height: 1.2;
+  }
+  @media screen and (min-width: 1200px) {
+    text-align: start;
+  }
+`;
+
+export const SubTitleHero = styled.p`
+  font-size: 18px;
+  line-height: 1.3;
+  margin-bottom: 24px;
+  padding: 0;
+
+  @media screen and (min-width: 768px) {
+    font-size: 22px;
+    line-height: 1.45;
+  }
+
+  @media screen and (min-width: 1200px) {
+    padding: 0;
+  }
 `;
