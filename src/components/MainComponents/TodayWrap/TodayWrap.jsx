f538a91322eac77899f55b7e614d3270acae3d39
erik465 <ehalasz06@gmail.com>
2023-12-17 00:03:41 +0100

    Styled Heading and dashboard link

--- src/components/MainComponents/TodayWrap/TodayWrap.jsx
+++ src/components/MainComponents/TodayWrap/TodayWrap.jsx
@@ -1,16 +1,43 @@
-import { Link } from 'react-router-dom';
 import DailyGoal from '../Daily goal/DailyGoal';
 import Water from '../Water/Water';
 import Food from '../Food/Food';
-import { StyledContainer } from './TodayWrap.styled';
+import {
+  StyledContainer,
+  HeadingWrapper,
+  StyledLink,
+} from './TodayWrap.styled';
 
 export default function TodayWrap() {
   return (
     <div>
-      <div>
+      <HeadingWrapper>
         <h2>Today</h2>
-        <Link to="/dashboard">On the way to the goal</Link>
-      </div>
+        <StyledLink to="/dashboard">
+          On the way to the goal{' '}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+          >
+            <path
+              d="M9.62012 3.95312L13.6668 7.99979L9.62012 12.0465"
+              stroke="#B6B6B6"
+              stroke-miterlimit="10"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M2.33301 8H13.553"
+              stroke="#B6B6B6"
+              stroke-miterlimit="10"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </StyledLink>
+      </HeadingWrapper>
       <StyledContainer>
         <DailyGoal />
         <Water />
--- src/components/MainComponents/TodayWrap/TodayWrap.styled.jsx
+++ src/components/MainComponents/TodayWrap/TodayWrap.styled.jsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const StyledContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 20px;
-  padding: 0 10px;
   margin-bottom: 20px;
 
   @media (min-width: 834px) {
@@ -12,4 +12,33 @@ export const StyledContainer = styled.div`
     flex-direction: row;
     flex-wrap: wrap;
   }
+`;
+
+export const HeadingWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+
+  h2 {
+    color: #fff;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1.25;
+  }
+`;
+
+export const StyledLink = styled(Link)`
+  color: #b6b6b6;
+  text-align: right;
+  font-family: Poppins;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 1.42;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  gap: 6px;
 `;
