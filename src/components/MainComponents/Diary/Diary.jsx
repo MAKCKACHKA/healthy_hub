fef70f27b660f67855e7cf5a0730da8ea9fc5d78
Anton <rodrigescouy25@gmail.com>
2023-12-17 14:10:11 +0200

    v1.0

--- src/components/MainComponents/Diary/Diary.jsx
+++ src/components/MainComponents/Diary/Diary.jsx
@@ -1,7 +1,18 @@
-export default function Diary() {
+import React from 'react';
+import snackImage from '../../../assets/meal.svg';
+
+const Diary = () => {
   return (
     <>
       <p>Diary</p>
+      <img
+        src={`${snackImage}#icon-Breakfast-image`}
+        alt="Snack Icon"
+        width="32"
+        height="32"
+      />
     </>
   );
-}
+};
+
+export default Diary;
