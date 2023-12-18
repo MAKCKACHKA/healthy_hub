fd88e83daa3a459185dd21572b0a6044f5925b9a
MAKCKACHKA <doubleakop@gmail.com>
2023-12-18 19:16:20 +0200

    added 2 emojie and headers private/public

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,132 @@
 import {
-  HeaderContainer,
-  Navigation,
-  StyledLink,
-  // IconWrapper,
+  // Navigation,
+  // StyledLink,
+  // HeaderContainer,
+  // NonAuthNav,
+  // NavLink,
+  HeaderContainer2,
+  LogoLink,
+  AvatarIcon,
+  DropIcon,
+  EditIcon,
+  ControlPanel,
+  InfoContainer,
+  ImageWrapper,
+  TextContainer,
+  EditableText,
+  AvatarContainer,
+  FunctionPanel,
+  KgSpan,
 } from './Header.styled';
 // import sprite from 'assets/sprite.svg';
+import icons from '../../assets/icons.svg';
+// import { useLocation } from 'react-router-dom';
+
+import run from '../../assets/emoji/run.png';
+import clap from '../../assets/emoji/clap.png';
 
 export const Header = () => {
+  // const location = useLocation();
+  // const isSignIn = location.pathname.includes('signin');
+  // const isSignUp = location.pathname.includes('signup');
+
+  const testInfo = {
+    name: 'Konstantin',
+    image: '',
+    goal: 'Gain Muscle',
+    weight: 75,
+    date: '05.06.2023',
+  };
+
   return (
-    <HeaderContainer>
-      <Navigation>
-        <StyledLink to="/welcome">
-          {/* <IconWrapper>
-            <use href={`${sprite}#icon-logo`} />
-          </IconWrapper> */}
-          WelcomePage
-        </StyledLink>
-        <StyledLink to="/signup">SignUpPage</StyledLink>
-        <StyledLink to="/signin">SignInPage</StyledLink>
-        <StyledLink to="/forgot-password">ForgotPasswordPage</StyledLink>
+    <HeaderContainer2>
+      <LogoLink to="/main">Your health</LogoLink>
+      <FunctionPanel>
+        <ControlPanel>
+          <InfoContainer>
+            <ImageWrapper>
+              <img src={run} alt="Running" />
+            </ImageWrapper>
+            <TextContainer>
+              <p>Goal</p>
+              <EditableText>
+                {testInfo.goal}
+                <DropIcon>
+                  <use href={`${icons}#icon-arrow-down`} />
+                </DropIcon>
+              </EditableText>
+            </TextContainer>
+          </InfoContainer>
+          <InfoContainer>
+            <ImageWrapper>
+              <div>
+                <img src={clap} alt="Clapping hands" />
+              </div>
+            </ImageWrapper>
+            <TextContainer>
+              <p>Weight</p>
+              <EditableText>
+                {testInfo.weight} <KgSpan>kg</KgSpan>
+                <EditIcon>
+                  <use href={`${icons}#icon-edit`} />
+                </EditIcon>
+              </EditableText>
+            </TextContainer>
+          </InfoContainer>
+        </ControlPanel>
+        <AvatarContainer>
+          <p>{testInfo.name}</p>
+          <AvatarIcon>
+            <use href={`${icons}#icon-profile-circle`} />
+          </AvatarIcon>
+          <DropIcon>
+            <use href={`${icons}#icon-arrow-down`} />
+          </DropIcon>
+        </AvatarContainer>
+      </FunctionPanel>
+    </HeaderContainer2>
+    // <HeaderContainer2>
+    //   {isSignUp ? (
+    //     <LogoLink to="/welcome">Your health</LogoLink>
+    //   ) : (
+    //     <LogoLink to="/welcome">HealthyHub</LogoLink>
+    //   )}
+    //   <NonAuthNav>
+    //     {isSignIn ? (
+    //       <NavLink style={{ color: '#E3FFA8' }} to="/signin">
+    //         Sign in
+    //       </NavLink>
+    //     ) : (
+    //       <NavLink to="/signin">Sign in</NavLink>
+    //     )}
+    //     <span> / </span>
+    //     {isSignUp ? (
+    //       <NavLink style={{ color: '#E3FFA8' }} to="/signup">
+    //         Sign up
+    //       </NavLink>
+    //     ) : (
+    //       <NavLink to="/signup">Sign up</NavLink>
+    //     )}{' '}
+    //     <AvatarIcon>
+    //       <use href={`${icons}#icon-profile-circle`} />
+    //     </AvatarIcon>
+    //   </NonAuthNav>
+    //   {/* <HeaderContainer>
+    //     <Navigation>
+    //     <StyledLink to="/welcome">WelcomePage</StyledLink>
+    //     <StyledLink to="/signup">SignUpPage</StyledLink>
+    //     <StyledLink to="/signin">SignInPage</StyledLink>
+    //     <StyledLink to="/forgot-password">ForgotPasswordPage</StyledLink>
 
-        <StyledLink to="/set-params">SetParamsPage</StyledLink>
+    //     <StyledLink to="/set-params">SetParamsPage</StyledLink>
 
-        <StyledLink to="/main">MainPage</StyledLink>
-        <StyledLink to="/dashboard">DashboardPage</StyledLink>
-        <StyledLink to="/diary">DiaryPage</StyledLink>
-        <StyledLink to="/recommended-food">RecommendedFoodPage</StyledLink>
-        <StyledLink to="/settings">SettingsPage</StyledLink>
-      </Navigation>
-    </HeaderContainer>
+    //     <StyledLink to="/main">MainPage</StyledLink>
+    //     <StyledLink to="/dashboard">DashboardPage</StyledLink>
+    //     <StyledLink to="/diary">DiaryPage</StyledLink>
+    //     <StyledLink to="/recommended-food">RecommendedFoodPage</StyledLink>
+    //     <StyledLink to="/settings">SettingsPage</StyledLink>
+    //   </Navigation>
+    //   </HeaderContainer> */}
+    // </HeaderContainer2>
   );
 };
--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 
 export const HeaderContainer = styled.div`
   padding: 20px;
+  @media screen and (min-width: 768px) {
+  }
+  @media screen and (min-width: 1200px) {
+  }
 `;
 
 export const Navigation = styled.nav`
@@ -15,12 +19,6 @@ export const Navigation = styled.nav`
 export const StyledLink = styled(Link)`
   padding: 8px 16px;
   color: var(--primary-text-color);
-  /* text-shadow:
-    0 0 5px #00baff,
-    0 0 10px #00baff,
-    0 0 20px #00baff,
-    0 0 40px #00baff,
-    0 0 80px #00baff; */
   transition:
     color 0.3s,
     text-shadow 0.3s;
@@ -28,25 +26,187 @@ export const StyledLink = styled(Link)`
   &:hover,
   &:focus {
     color: #00baff;
-    /* text-shadow:
-      0 0 5px #00baff,
-      0 0 10px #00baff,
-      0 0 20px #00baff,
-      0 0 40px #00baff,
-      0 0 80px #00baff; */
+  }
+`;
+export const HeaderContainer2 = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid #000;
+  background: var(--layout-color);
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  padding: 18px 10px;
+  align-items: center;
+
+  @media screen and (min-width: 768px) {
+    padding: 26px 27px;
+  }
+  @media screen and (min-width: 1200px) {
+    padding: 26px 34px;
   }
 `;
 
-export const IconWrapper = styled.svg`
-  width: ${(props) => props.size || '12px'};
-  height: ${(props) => props.size || '12px'};
-  fill: ${(props) => props.color || '#ffffff'};
-  display: inline-block;
-  margin-right: 5px;
-  /* box-shadow:
-    0 0 5px #00baff,
-    0 0 10px #00baff,
-    0 0 20px #00baff,
-    0 0 40px #00baff,
-    0 0 80px #00baff; */
+export const NonAuthNav = styled.nav`
+  display: flex;
+  stroke: var(--primary-title-text-color);
+  align-items: center;
+  gap: 3px;
+`;
+
+export const LogoLink = styled(Link)`
+  color: var(--primary-text-color);
+  font-family: Poppins;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 1.37;
+  cursor: pointer;
+  transition: color 0.3s;
+
+  @media screen and (min-width: 768px) {
+    font-size: 22px;
+    line-height: 1.45;
+  }
+
+  &:hover,
+  &:focus {
+    color: var(--primary-title-text-color);
+  }
+`;
+export const NavLink = styled(Link)`
+  cursor: pointer;
+  color: var(--primary-title-text-color);
+  text-align: right;
+  font-family: Poppins;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.33;
+  transition: color 0.3s;
+  &:hover,
+  &:focus {
+    color: var(--primary-text-color);
+  }
+  @media screen and (min-width: 768px) {
+    font-size: 14px;
+    line-height: 1.42;
+  }
+`;
+export const AvatarIcon = styled.svg`
+  margin-left: 6px;
+  width: 28px;
+  height: 28px;
+  /* fill: var(--primary-text-color); */
+  stroke: var(--primary-title-text-color);
+`;
+
+export const AuthNav = styled.div``;
+
+export const UserInfoNav = styled.div``;
+
+export const DropIcon = styled.svg`
+  width: 14px;
+  height: 14px;
+  margin-left: 12px;
+  stroke: var(--primary-btn-color);
+`;
+export const EditIcon = styled.svg`
+  display: flex;
+  width: 16px;
+  height: 16px;
+  justify-content: center;
+  align-items: center;
+  stroke: var(--primary-btn-color);
+`;
+export const ControlPanel = styled.div`
+  align-items: center;
+  display: none;
+
+  @media screen and (min-width: 768px) {
+    gap: 40px;
+    display: flex;
+  }
+  @media screen and (min-width: 1200px) {
+    gap: 80px;
+  }
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const ImageWrapper = styled.div`
+  width: 48px;
+  height: 48px;
+  padding: 10px;
+  img {
+    width: 100%;
+    border-radius: 5px;
+  }
+  border-radius: 12px;
+  border: 1px solid #5f5e5c;
+  background: rgba(255, 255, 255, 0.01);
+`;
+
+export const TextContainer = styled.div`
+  p {
+    color: var(--primary-title-text-color);
+    font-size: 14px;
+    font-weight: 600;
+    line-height: 1.42;
+    font-style: normal;
+  }
+`;
+
+export const EditableText = styled.p`
+  display: flex;
+  align-items: center;
+  span {
+    margin-left: 5px;
+  }
+`;
+
+export const AvatarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  svg {
+    margin-left: 4px;
+  }
+  p {
+    color: var(--primary-title-text-color);
+    text-align: right;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1.33;
+  }
+  @media screen and (min-width: 768px) {
+    p {
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 1.42;
+    }
+  }
+`;
+export const FunctionPanel = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media screen and (min-width: 768px) {
+    gap: 75px;
+  }
+  @media screen and (min-width: 1200px) {
+    gap: 124px;
+  }
+`;
+export const KgSpan = styled.span`
+  color: var(--primary-text-color);
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.42;
+  margin-right: 12px;
 `;
