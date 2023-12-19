4b91e7373e84d4478bfd6fa398308e0f8aa87461
DenisZaviriukha <dv.zavirukha@gmail.com>
2023-12-19 21:34:51 +0200

    init spinner commit
    - add spinner with no styles

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,13 @@
+// import { Backdrop, Spinner } from "./Spinner.styled"
+// import { MoonLoader } from 'react-spinners';
+
+
+// export const Spinner = isLoading => {
+
+//     return (
+        // isLoading && <MoonLoader color={'#e3ffa8'} loading={isLoading} />
+        // <Backdrop>
+        //     <Spinner />
+        // </Backdrop>       
+//     )
+// }
