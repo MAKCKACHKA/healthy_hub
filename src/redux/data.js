e5da63c583818fcb927ce2bf1bf98e57479bc002
erik465 <ehalasz06@gmail.com>
2023-12-20 17:42:51 +0100

    added user data slice, reducer and selectors

---
+++ src/redux/data.js
@@ -0,0 +1,73 @@
+import { createSlice } from '@reduxjs/toolkit';
+import {
+  getMonthlyStatistics,
+  getCurrentUser,
+  updateUserInformation,
+  updateUserGoal,
+  addUserWeight,
+  addFoodIntake,
+  deleteFoodIntake,
+  addWaterIntake,
+  deleteWaterIntake,
+  updateFoodIntake,
+  addUserAvatar,
+} from './operations';
+
+const initialState = {
+  data: {},
+  statistics: {},
+  isLoading: false,
+  error: null,
+};
+
+const handlePending = (state) => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
+const dataSlice = createSlice({
+  name: 'data',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getMonthlyStatistics.pending, handlePending)
+      .addCase(getMonthlyStatistics.rejected, handleRejected)
+      .addCase(getMonthlyStatistics.fulfilled, (state, action) => {
+        state.statistics = action.payload;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(getCurrentUser.pending, handlePending)
+      .addCase(getCurrentUser.rejected, handleRejected)
+      .addCase(getCurrentUser.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(updateUserInformation.pending, handlePending)
+      .addCase(updateUserInformation.rejected, handleRejected)
+      .addCase(updateUserGoal.pending, handlePending)
+      .addCase(updateUserGoal.rejected, handleRejected)
+      .addCase(addUserWeight.pending, handlePending)
+      .addCase(addUserWeight.rejected, handleRejected)
+      .addCase(addFoodIntake.pending, handlePending)
+      .addCase(addFoodIntake.rejected, handleRejected)
+      .addCase(deleteFoodIntake.pending, handlePending)
+      .addCase(deleteFoodIntake.rejected, handleRejected)
+      .addCase(addWaterIntake.pending, handlePending)
+      .addCase(addWaterIntake.rejected, handleRejected)
+      .addCase(deleteWaterIntake.pending, handlePending)
+      .addCase(deleteWaterIntake.rejected, handleRejected)
+      .addCase(updateFoodIntake.pending, handlePending)
+      .addCase(updateFoodIntake.rejected, handleRejected)
+      .addCase(addUserAvatar.pending, handlePending)
+      .addCase(addUserAvatar.rejected, handleRejected);
+  },
+});
+
+export const dataReducer = dataSlice.reducer;
--- src/redux/selesctors.js
+++ src/redux/selesctors.js
@@ -1,1 +1,9 @@
 export const selectToken = (state) => state.auth.token;
+
+export const selectUserStatistics = (state) => state.data.statistics;
+
+export const selectUserData = (state) => state.data.data;
+
+export const selectUserMeals = (state) => state.data.data.consumedMealsByDay;
+
+export const selectDailyNutrition = (state) => state.data.data.dailyNutrition;
